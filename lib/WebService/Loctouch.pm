5192d1982920b3446cbc4db1854f926759d064bf
Kashima Takumi <kodokurider@gmail.com>
2012-09-30 17:00:41 +0900

    add spots method and spots_photo method

--- lib/WebService/Loctouch.pm
+++ lib/WebService/Loctouch.pm
@@ -2,14 +2,14 @@ package WebService::Loctouch;
 use strict;
 use warnings;
 use LWP::UserAgent;
+use Carp qw/croak/;
 use URI;
 use JSON;
-use Data::Dumper;
 
 sub new {
     my ($class, %args) = @_;
     my $self = bless {
-        baseurl => 'https://api.loctouch.com/v1/',
+        baseurl => 'https://api.loctouch.com/v1',
         ua => LWP::UserAgent->new,
     }, $class;
     return $self;
@@ -17,12 +17,32 @@ sub new {
 
 sub spots_search {
     my ($self, %args) = @_;
-    my $url = URI->new($self->{baseurl}.'spots/search');
+    my $url = URI->new($self->{baseurl} . '/spots/search');
     $url->query_form(%args);
     my $r = $self->{ua}->get($url->as_string);
     my $content = decode_json($r->content);
     return $content unless ($r->is_success && $content->{code} == 200);
-    return @{ $content->{spots} };
+    return $content->{spots};
+}
+
+sub spots {
+    my ($self, %args) = @_;
+    croak 'Error: spots method required spot id args' unless $args{id};
+    my $url = URI->new($self->{baseurl} . '/spots/' . $args{id});
+    my $r = $self->{ua}->get($url->as_string);
+    my $content = decode_json($r->content);
+    return $content unless ($r->is_success && $content->{code} == 200);
+    return $content->{spot};
+}
+
+sub spots_photos {
+   my ($self, %args) = @_;
+    croak 'Error: spots method required spot id args' unless $args{id};
+    my $url = URI->new($self->{baseurl} . '/spots/' . $args{id} . '/photos');
+    my $r = $self->{ua}->get($url->as_string);
+    my $content = decode_json($r->content);
+    return $content unless ($r->is_success && $content->{code} == 200);
+    return $content->{photos};
 }
 
 1;
--- t/WebService-Loctouch.t
+++ t/WebService-Loctouch.t
@@ -4,20 +4,41 @@ use warnings;
 use base 'Test::Class';
 use Test::More;
 use WebService::Loctouch;
-use Data::Dumper;
 
-sub startup : Test(startup => 1) {
+sub startup : Test(startup => 2) {
     use_ok 'WebService::Loctouch';
+    ok my $loc = WebService::Loctouch->new;
 }
 
 sub spots_search : Test(2) {
-    ok my $loc = WebService::Loctouch->new;
-    my @spots =  $loc->spots_search(
+    my $loc = WebService::Loctouch->new;
+    my @spots;
+
+    @spots =  @{ $loc->spots_search(
         'category_id' => '140',
         'lat' => '34.341927',
         'lng' => '134.054765',
-    );
+    ) };
+
     ok $spots[0]{name};
+
+    @spots =  @{ $loc->spots_search(
+        'query' => 'cafe',
+        'limit' => 10,
+    ) };
+
+    ok $spots[0]{name};
+}
+
+sub spot : Test(3) {
+    my @spots;
+    my $loc = WebService::Loctouch->new;
+
+    my %spot =  %{ $loc->spots('id' => 140) };
+    ok $spot{name};
+    my @photo =  @{ $loc->spots_photos('id' => 140) };
+    is $spot{name}, $photo[0]{touch}{spot}{name};
+    ok $photo[0]{touch}{photo}{small};
 }
 
 __PACKAGE__->runtests;
